d06ab4df63431a3bc6b1ca5a3b34c8bf3276df82
emvk09 <edwinmoncy@gmail.com>
2023-06-29 20:45:07 +0530

    Feat: Initial configuration

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>hello</h1>
+      <p>poda</p>
+    </div>
+  );
+};
+
+export default App;
