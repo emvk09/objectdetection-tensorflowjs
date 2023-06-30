a45dc11a471b972d5061731afccf1f1695c99786
emvk09 <edwinmoncy@gmail.com>
2023-07-01 02:20:37 +0530

    Random colors for rectangles and changes to variable names

--- src/App.jsx
+++ src/App.jsx
@@ -2,93 +2,87 @@ import React, { useState, useEffect, useRef } from "react";
 import * as tf from "@tensorflow/tfjs";
 import * as cocossd from "@tensorflow-models/coco-ssd";
 import Webcam from "react-webcam";
+import { drawRectangle } from "./DrawingUtilities";
 import "./App.css";
 
 const App = () => {
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
 
-  const runCoco = async () => {
-    try {
-      // load newtwork in to the model
-      const network = await cocossd.load();
+  useEffect(() => {
+    runCoco();
+  }, []);
 
-      // time delay between looping different things and detecting them
+  async function runCoco() {
+    try {
+      // this loads the coco ssd model
+      const loadedCocoModel = await cocossd.load();
+
+      // setInterval sets up a continuous detection loop. The loaded model is passed to the function which does the detection from webcam
       setInterval(() => {
-        detect(network);
+        processVideoUsingModel(loadedCocoModel);
       }, 10);
     } catch (error) {
       console.log(error);
     }
-  };
+  }
 
-  const detect = async (network) => {
-    // check if data is available
+  const processVideoUsingModel = async (loadedCocoModel) => {
+    // check if webcam data is available and video is availabele for processing
     if (
       typeof webcamRef.current !== "undefined" &&
       webcamRef.current !== null &&
       webcamRef.current.video.readyState === 4
     ) {
-      // get the video properties
+      // get the video and its properties
       const video = webcamRef.current.video;
       const videoWidth = webcamRef.current.video.videoWidth;
       const videoHeight = webcamRef.current.video.videoHeight;
 
-      // Set video width
+      // Set video to be displayed based on the actual width and height of the direct video from camera
       webcamRef.current.video.width = videoWidth;
       webcamRef.current.video.height = videoHeight;
 
-      // Set canvas height and width
+      // Set canvas to be displayed based on the actual width and height of the direct video from camera
       canvasRef.current.width = videoWidth;
       canvasRef.current.height = videoHeight;
 
-      // Make detections
-      const obj = await network.detect(video);
-      console.log(obj);
+      // The video feed is passed to the model. The predicted results are returned to resultObjs. It is an array of objects
+      const resultObjs = await loadedCocoModel.detect(video);
+      console.log(resultObjs);
 
-      // Draw mesh
-      const ctx = canvasRef.current.getContext("2d");
+      // 2D rendering context (the area to be drawn) of the canvas element is taken
+      const canvas = canvasRef.current.getContext("2d");
+
+      // pass the predicted object and canvas to the drawRectangle function
+      drawRectangle(resultObjs, canvas);
     }
   };
 
-  useEffect(() => {
-    runCoco();
-  }, []);
-
   return (
     <div className="App">
-      <header className="App-header">
-        <Webcam
-          ref={webcamRef}
-          muted={true}
-          style={{
-            position: "absolute",
-            marginLeft: "auto",
-            marginRight: "auto",
-            left: 0,
-            right: 0,
-            textAlign: "center",
-            zindex: 9,
-            width: 640,
-            height: 480,
-          }}
-        />
+      <Webcam
+        ref={webcamRef}
+        muted={true} // this mutes the audio feed
+        style={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          zIndex: "1",
+        }}
+      />
 
-        <canvas
-          ref={canvasRef}
-          style={{
-            position: "absolute",
-            marginLeft: "auto",
-            marginRight: "auto",
-            left: 0,
-            right: 0,
-            textAlign: "center",
-            zindex: 8,
-            width: 640,
-            height: 480,
-          }}
-        />
-      </header>
+      <canvas
+        ref={canvasRef}
+        style={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          zIndex: "10",
+        }}
+      />
     </div>
   );
 };
---
+++ src/DrawingUtilities.js
@@ -0,0 +1,26 @@
+export const drawRectangle = (resultObjs, canvas) => {
+  resultObjs.forEach((result) => {
+    // get predictions results
+    const [x, y, width, height] = result.bbox; // here we are destructuring an array
+    const { class: detectedClassText } = result; // here we are destructuring an object. class is a reserved word. So we assign another name to it
+
+    // Math.random() generates a random decimal number between 0 and 1
+    // Thus we get a random decimal number between 0 and 16777215 after multiplication
+    // Math.floor() rounds down the resulting decimal number
+    const color = "#" + Math.floor(Math.random() * 16777215).toString();
+    // strokeStyle represents stroke (border) color for the rectangle
+    canvas.strokeStyle = color;
+    canvas.font = "18px Arial";
+    // represents the color for the text
+    canvas.fillStyle = color;
+
+    // starts a new path for drawing
+    canvas.beginPath();
+    // this draws the text on the canvas at the coordinates (x, y)
+    canvas.fillText(detectedClassText, x, y);
+    // this creates a rectangle on the canvas using the coordinates and dimensions
+    canvas.rect(x, y, width, height);
+    // this draws the rectangle border (stroke) on the canvas
+    canvas.stroke();
+  });
+};
